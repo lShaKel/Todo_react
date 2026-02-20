6132467a98b0a43c11c061579b8f7d6829566a8f
ShaKe <buturlakinaleksandr56@gmail.com>
2026-02-20 23:15:42 +0200

    Initial structure

---
+++ src/components/Todo.jsx
@@ -0,0 +1,19 @@
+import AddTaskForm from "./AddTaskForm.jsx";
+import SearchTaskForm from "./SearchTaskForm.jsx";
+import TodoInfo from "./TodoInfo.jsx";
+import TodoList from "./TodoList.jsx";
+
+const Todo = () => {
+  return (
+    <div className="todo">
+      <h1 className="todo__title">To Do List</h1>
+      <AddTaskForm />
+      <SearchTaskForm />
+      <TodoInfo />
+      <TodoList />
+      <div className="todo__empty-message"></div>
+    </div>
+  )
+}
+
+export default Todo
